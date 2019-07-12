51c56b87f5d78b2f52665efe8d84a55db5997c42
igor-pahomov <igor.pahomov@zensoft.io>
2019-07-12 12:24:46 +0300

    add wallet controller test, base controller test

--- src/test/kotlin/io/openfuture/state/controller/AccountControllerTest.kt
+++ src/test/kotlin/io/openfuture/state/controller/AccountControllerTest.kt
@@ -1,34 +1,22 @@
 package io.openfuture.state.controller
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.openfuture.state.entity.Account
 import io.openfuture.state.service.AccountService
 import io.openfuture.state.service.WalletService
 import io.openfuture.state.util.any
 import io.openfuture.state.util.createDummyAccount
 import io.openfuture.state.util.readResource
-import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 import org.mockito.BDDMockito.anySet
 import org.mockito.BDDMockito.given
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
-import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import org.springframework.test.web.servlet.setup.MockMvcBuilders
-import org.springframework.web.context.WebApplicationContext
 
 @WebMvcTest(AccountController::class)
-@RunWith(SpringRunner::class)
-class AccountControllerTest {
-
-    val objectMapper: ObjectMapper = jacksonObjectMapper()
+class AccountControllerTest : BaseControllerTest() {
 
     @MockBean
     private lateinit var accountService: AccountService
@@ -36,17 +24,6 @@ class AccountControllerTest {
     @MockBean
     private lateinit var walletService: WalletService
 
-    @Autowired
-    lateinit var mockMvc: MockMvc
-
-    @Autowired
-    lateinit var webApplicationContext: WebApplicationContext
-
-
-    @Before
-    fun initMockMvc() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build()
-    }
 
     @Test
     fun createAccountTest() {
@@ -62,7 +39,7 @@ class AccountControllerTest {
     }
 
     @Test
-    fun get() {
+    fun getAccountByIdTest() {
         val account = createDummyAccount().apply { id = 1 }
 
         given(accountService.get(account.id)).willReturn(account)
@@ -72,7 +49,7 @@ class AccountControllerTest {
     }
 
     @Test
-    fun update() {
+    fun updateAccountWebHookTest() {
         val requestBody = readResource("updateAccountWebHookRequest.json", javaClass)
         val account = createDummyAccount().apply { id = 1 }
 
