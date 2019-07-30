81d56bb07822c108148c4f319c2543cf9ff78597
George Bisiarin <george.bisiarin@zensoft.io>
2019-07-30 19:14:05 +0300

    security basic

--- src/main/kotlin/io/openfuture/state/config/CORSConfig.kt
+++ src/main/kotlin/io/openfuture/state/config/CORSConfig.kt
@@ -3,13 +3,26 @@ package io.openfuture.state.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
-
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 
 @Configuration
-class CORSConfig {
+class CORSConfig: WebSecurityConfigurerAdapter() {
+
+    @Throws(Exception::class)
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+    }
+
+    @Throws(Exception::class)
+    override fun configure(http: HttpSecurity) {
+        http.authorizeRequests().anyRequest().authenticated()
+    }
 
     @Bean
     fun corsFilter(@Value("\${openfuture.api.url}") apiUrl: String): CorsFilter {
