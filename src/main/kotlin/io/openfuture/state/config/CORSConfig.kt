b5afb6a2d83975879de28c5fd72085463fe62008
George Bisiarin <george.bisiarin@zensoft.io>
2019-07-31 16:50:37 +0300

    security basic auth

--- src/main/kotlin/io/openfuture/state/config/CORSConfig.kt
+++ src/main/kotlin/io/openfuture/state/config/CORSConfig.kt
@@ -9,19 +9,30 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.filter.CorsFilter
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @Configuration
+@EnableWebSecurity
 class CORSConfig: WebSecurityConfigurerAdapter() {
 
     @Throws(Exception::class)
     override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER")
+        val passwordEncoder = BCryptPasswordEncoder()
+        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder)
+                .withUser("user").password(passwordEncoder.encode("password")).roles("USER")
     }
 
     @Throws(Exception::class)
     override fun configure(http: HttpSecurity) {
-        http.authorizeRequests().anyRequest().authenticated()
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable()
     }
 
     @Bean
