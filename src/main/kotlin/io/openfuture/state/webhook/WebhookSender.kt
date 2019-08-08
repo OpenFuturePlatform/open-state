c0f348c8ccbbac86f1513a587858b689d11a42e1
igor-pahomov <igor.pahomov@zensoft.io>
2019-08-08 18:33:38 +0300

    check empty webhook

--- src/main/kotlin/io/openfuture/state/webhook/WebhookSender.kt
+++ src/main/kotlin/io/openfuture/state/webhook/WebhookSender.kt
@@ -2,7 +2,6 @@ package io.openfuture.state.webhook
 
 import io.openfuture.state.controller.domain.dto.TrackingTransactionDto
 import io.openfuture.state.entity.Transaction
-import org.apache.commons.validator.routines.UrlValidator
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 
@@ -11,7 +10,7 @@ class WebhookSender {
 
     fun sendWebHook(urlList: List<String>, transaction: Transaction) {
         urlList.forEach {
-            if (UrlValidator().isValid(it)) {
+            if (it.isNotEmpty()) {
                 RestTemplate().postForLocation(it, TrackingTransactionDto(transaction))
             }
         }
