4833f629dfcd31fb5ae2b8e3cd697f89792706e1
Temirlan <temirlan.renatov>
2020-09-09 21:00:03 +0600

    empty to blank #7

--- src/main/kotlin/io/openfuture/state/property/EthereumProperties.kt
+++ src/main/kotlin/io/openfuture/state/property/EthereumProperties.kt
@@ -3,10 +3,10 @@ package io.openfuture.state.property
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.validation.annotation.Validated
-import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "ethereum")
 @Validated
-data class EthereumProperties(@field:NotNull @field:NotEmpty val nodeAddress: String?)
+data class EthereumProperties(@field:NotNull @field:NotBlank val nodeAddress: String?)
