d8a006ca66e304badcc7cb29ee5c9cd81c147dfa
Ruslan Molchanov <ruslanys@gmail.com>
2020-09-10 23:09:34 +0300

    Test fix

--- src/test/kotlin/io/openfuture/state/repository/ProcessingRedisRepositoryTest.kt
+++ src/test/kotlin/io/openfuture/state/repository/ProcessingRedisRepositoryTest.kt
@@ -31,6 +31,7 @@ class ProcessingRedisRepositoryTest : RedisRepositoryTests() {
             it.serverCommands().flushAll()
         }.blockFirst()
         given(blockchain.getName()).willReturn("MockBlockchain")
+        given(blockchain.toString()).willReturn(blockchain.getName())
     }
 
     @Test
