242dba268e06010110ba29315bbb98ce1fda36c9
borbuevbeksultan <beksultan.borbuev@betpawa.com>
2022-03-09 22:50:54 +0600

    SA-19 Implement webhook data processing

--- src/main/kotlin/io/openfuture/state/controller/OrderController.kt
+++ src/main/kotlin/io/openfuture/state/controller/OrderController.kt
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController("/api/orders")
 class OrderController(
     val orderRepository: OrderRepository
 ) {
