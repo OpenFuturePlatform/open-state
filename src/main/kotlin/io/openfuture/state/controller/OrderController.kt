11c856800f99718ad8be7e49150d4d45af1e5301
borbuevbeksultan <beksultan.borbuev@betpawa.com>
2022-03-09 22:48:27 +0600

    SA-19 Implement webhook data processing

--- src/main/kotlin/io/openfuture/state/controller/OrderController.kt
+++ src/main/kotlin/io/openfuture/state/controller/OrderController.kt
@@ -1,15 +1,20 @@
 package io.openfuture.state.controller
 
+import io.openfuture.state.domain.Order
+import io.openfuture.state.repository.OrderRepository
+import kotlinx.coroutines.reactive.awaitSingle
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class OrderController {
+class OrderController(
+    val orderRepository: OrderRepository
+) {
 
     @GetMapping("/{orderId}")
-    fun getOrderState(@PathVariable orderId: String) {
-
+    suspend fun getOrderState(@PathVariable orderId: String): Order {
+        return orderRepository.findAllByOrderId(orderId).awaitSingle()
     }
 
 }
